0ff6a93cc27aa70d0edc259fc3cff53a32fc1acd
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2022-11-04 14:00:37 -0600

    Add more cmp sources, add cmp keybindings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,17 @@
--- local status_ok, cmp = require("core.loader")("cmp")
 local status_ok, cmp = pcall(require, "cmp")
 if not status_ok then
   return
 end
 
 local l_status_ok, luasnip = pcall(require, "luasnip")
--- local _ = require("core.loader").l()
--- local l_status_ok, luasnip = pcall(require, "luasnipa")
 if not l_status_ok then
   return
 end
 
+local check_backspace = function()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
 
 cmp.setup({
 	snippet = {
@@ -19,7 +20,52 @@ cmp.setup({
 		end,
 	},
   sources = {
+    {name = "nvim_lsp"},
+    {name = "nvim_lua"},
     {name = "buffer"},
     {name = "luasnip"},
+    {name = "path"},
   },
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		-- Accept currently selected item. If none selected, `select` first item.
+		-- Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif check_backspace() then
+				fallback()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+	}),
 })
