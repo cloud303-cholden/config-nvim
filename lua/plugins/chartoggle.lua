0743aabaf0e68a8b1da377004d9084d9029e02b3
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2022-12-29 11:28:50 -0700

    Add which-key registration to chartoggle

--- lua/plugins/chartoggle.lua
+++ lua/plugins/chartoggle.lua
@@ -10,6 +10,27 @@ M.load = function()
     leader = " ",
     keys = { ",", ";" },
   })
+
+  local wk = require("which-key")
+  local opts = {
+    mode = "n",
+    prefix = "<leader>",
+    buffer = nil,
+    silent = false,
+    noremap = true,
+    nowait = true,
+  }
+  local mappings = {
+    [","] = {
+      ":lua require('chartoggle').toggle(',')<CR>",
+      "Toggle Comma",
+    },
+    [";"] = {
+      ":lua require('chartoggle').toggle(';')<CR>",
+      "Toggle Semicolon",
+    },
+  }
+  wk.register(mappings, opts)
 end
 
 return M
