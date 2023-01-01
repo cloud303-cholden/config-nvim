db8545ac8e0213e7f307edbca5a7010ad47e31fa
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-01-01 11:22:51 -0700

    Add angled bracket autopairs for rust files

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -7,6 +7,13 @@ M.load = function()
   end
 
   autopairs.setup({})
+
+  local rule = require("nvim-autopairs.rule")
+  local conds = require("nvim-autopairs.conds")
+
+  autopairs.add_rules({
+    rule("<", ">", "rust"):with_pair(conds.not_before_text(" ")),
+  })
 end
 
 return M
