7ad86dd84ad36992936573833d470904e347fb4a
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-04-29 16:26:19 -0600

    Remap open keymap

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -9,6 +9,28 @@ M.load = function()
       },
     },
   })
+
+  local function my_on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+  end
+
+  require("nvim-tree").setup({
+    on_attach = my_on_attach,
+  })
+
   local wk = require("which-key")
   local opts = {
     mode = "n",
