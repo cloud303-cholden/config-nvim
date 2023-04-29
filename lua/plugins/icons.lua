ad21cb39c7d4207907d02cae344b8d5a4bedb8b1
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-04-29 15:57:51 -0600

    Remove material icons config

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -2,19 +2,6 @@ local M = {}
 
 M.load = function()
   require("nvim-nonicons").setup({})
-  -- local web_devicons_ok, web_devicons = pcall(require, "nvim-web-devicons")
-  -- if not web_devicons_ok then
-  --   return
-  -- end
-  --
-  -- local material_icon_ok, material_icon = pcall(require, "nvim-material-icon")
-  -- if not material_icon_ok then
-  --   return
-  -- end
-  --
-  -- web_devicons.setup({
-  --   override = material_icon.get_icons(),
-  -- })
 end
 
 return M
