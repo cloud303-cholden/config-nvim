6e8f21d3f329953eaa75f6a4d2e0ad1eb28ecdc8
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-04-29 14:02:35 -0600

    Update icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -26,27 +26,27 @@ M.load = function()
   dashboard.section.header.val = header()
 
   dashboard.section.buttons.val = {
-    dashboard.button("r", "  Repositories", ":Telescope projects<CR>"),
-    dashboard.button("p", "ஃ  Plugins", ":Lazy<CR>"),
-    dashboard.button("l", "↯  LSP", ":Mason<CR>"),
-    dashboard.button("c", "  Configuration", ":e $NVIM_CONFIG <CR>"),
-    dashboard.button("h", "  Help", ":h<CR>"),
-    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    dashboard.button("r", "  Repositories", ":Telescope projects<CR>"),
+    dashboard.button("p", "  Plugins", ":Lazy<CR>"),
+    dashboard.button("l", "  LSP", ":Mason<CR>"),
+    dashboard.button("c", "  Configuration", ":e $NVIM_CONFIG <CR>"),
+    dashboard.button("h", "  Help", ":h<CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
   }
 
   local function footer()
     local total_plugins = #vim.tbl_keys(require("lazy.core.config").plugins)
     local datetime = os.date("%m-%d-%Y %H:%M:%S")
-    local plugins_text = "   "
-      .. total_plugins
-      .. " plugins"
-      .. "   v"
+    local plugins_text = "   v"
       .. vim.version().major
       .. "."
       .. vim.version().minor
       .. "."
       .. vim.version().patch
-      .. "   "
+      .. "   "
+      .. total_plugins
+      .. " plugins"
+      .. "   "
       .. datetime
 
     return plugins_text
