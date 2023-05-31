c1af77e7a0bda9bd29d87f3291465b3e83a369bc
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2023-05-30 20:58:51 -0600

    Fix TF LSP settings

--- lua/plugins/lsp/settings/tf.lua
+++ lua/plugins/lsp/settings/tf.lua
@@ -2,6 +2,8 @@ local M = {}
 
 M.settings = {
   cmd = { "terraform-ls", "serve" },
+  filetypes = { "terraform", "terraform-vars", "tf" },
+  root_dir = require("lspconfig").util.root_pattern("*.tf", "*.tfvars"),
 }
 
 M.load = function()
